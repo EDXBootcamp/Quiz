b5a396a24d80f05d2f23d2f30bad97d6b3d73fd6
Tj Thouhid <tjthouhid@gmail.com>
2024-01-02 09:37:36 +0000

    added question array

---
+++ assets/js/questions.js
@@ -0,0 +1,19 @@
+var questions = [
+    {
+      question: "What is the correct syntax for creating a JavaScript function?",
+      choices: ["function = myFunction()", "function: myFunction()", "function myFunction()", "myFunction(): function"],
+      correctAnswer: "function myFunction()"
+    },
+    {
+      question: "Which method is used to add a new element at the end of an array?",
+      choices: ["push()", "append()", "addToEnd()", "insertLast()"],
+      correctAnswer: "push()"
+    },
+    {
+      question: "What does CSS stand for?",
+      choices: ["Counter Style Sheets", "Colorful Style Sheets", "Computer Style Sheets", "Cascading Style Sheets"],
+      correctAnswer: "Cascading Style Sheets"
+    },
+   
+  ];
+  
